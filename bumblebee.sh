e83dbe7e6c6adebcaabf9fac1a32156bc47810fa
Oleg Dolgikh <aristarkh@aristarkh.net>
2017-02-04 19:24:38 +0500

    Some improvements

--- bumblebee.sh
+++ bumblebee.sh
@@ -2,15 +2,16 @@
 
 if lspci | egrep 'VGA|3D' | grep -q Intel && lspci | egrep 'VGA|3D' | grep -q NVIDIA
 then
-local opt='n'
-read -p 'Do you want to install Bumblebee? (y/N)' opt && echo
+    opt='n'
+    read -p 'Do you want to install Bumblebee? (y/N)' opt && echo
     if [[ ${opt} = y ]]
     then
         if dpkg -s nvidia-prime &> /dev/null
         then
             echo 'Removing nvidia-prime...'
-            apt-get -y purge nvidia-prime
+            apt purge -y nvidia-prime
         fi
+    apt update
     apt install -y nvidia-331 nvidia-settings bumblebee bumblebee-nvidia primus primus-libs:i386"
     fi
 fi
--- samba.sh
+++ samba.sh
@@ -15,7 +15,5 @@ cat << EOF >> "${smb_config}"
     read only = yes
     guest ok = yes
 EOF
-# echo -e '[Misc$]\n   path = /windows/misc\n   read only = no\n   guest ok = yes\n' >> "${smb_config}"
-# echo -e '[Misc]\n   comment = Misc\n   path = /windows/misc/Misc\n   read only = yes\n   guest ok = yes\n' >> "${smb_config}"
 service nmbd restart
 service smbd restart
